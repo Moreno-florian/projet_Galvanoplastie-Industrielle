834c4f293db3c915964a774599f64abc6fa8e3de
Moreno-florian <62134463+Moreno-florian@users.noreply.github.com>
2020-08-04 12:21:34 +0200

    Add files via upload

--- controllerCustomer.php
+++ controllerCustomer.php
@@ -3,27 +3,41 @@ require_once '../Model/Dao/dbConnect.php';
 require_once '../Model/customerClass.php';
 require_once '../Model/Dao/daoCustomer.php';
 
+//switch (key($_GET)) {
+        /*****************************************************************Add Name_Customer************************************************************************* */
+    //case 'formAddCustomer':
 
-/*****************************************************************Add Name_Customer************************************************************************* */
-if (isset($_POST['Name'])) {
+        if (isset($_POST['Name'])) {
 
-    if (!empty($_POST["Name"])) {  // If an important field is empty
+            if (!empty($_POST["Name"])) {  // If an important field is empty
 
-        $newCustomer = new Customer(null, addslashes(htmlspecialchars(ucfirst($_POST["Name"]))));
-        (new daoCustomer())->addCustomer($newCustomer);
-        header('Location: ../View/formAddCustomer.phtml');
-    } else {
+                $newCustomer = new Customer(null, addslashes(htmlspecialchars(ucfirst($_POST["Name"]))));
+                (new daoCustomer())->addCustomer($newCustomer);
+                header('Location: ../View/formAddCustomer.phtml');
+            } else {
 
-        echo ('Le champ est vide est le \'required\' à était désactivé !');
-        header("refresh:3;url=../View/formAddCustomer.phtml"); // if requiered is disabled
+                echo ('Le champ est vide est le \'required\' à était désactivé !');
+                header("refresh:3;url=../View/formAddCustomer.phtml"); // if 'requiered' in HTML is disabled
+            }
+        }
+       // break;
+        /********************************************************************************************************************************************************* */
+    //case 'showCustomer':
+        
+        $list = (new DaoCustomer())->showCustomer();    // Call function showCustomer() from "Dao/daoCustomer.php"
+        include_once "./formShowCustomer.phtml";
+        header('./formShowCustomer.phtml');
+
+       // break;
+        
+//}
 
 
-    }
-}
-/********************************************************************************************************************************************************* */
 
-/*$list = (new daoCustomer())->showCustomer();    // Call function showStadiumList() from "Dao/daoStadium.php"
-var_dump($list);*/
+
+
+/*$list=(new DaoCustomer())->showCustomer();
+include_once '../View/formShowCustomer.phtml';*/
 
 //$list2 = (new DaoCustomer())->modifyCustomer();
 
